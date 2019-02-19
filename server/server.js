1c806d9fafb572d81b3afc3f2464556b94b37ffe
Alex Blostein <AlexBlostein@Alexs-MacBook-Air-3.local>
2019-02-19 15:19:23 -0500

    Add a GET

--- server/server.js
+++ server/server.js
@@ -35,8 +35,20 @@ app.post('/todos',(req,res) => {
 	});
 });
 
+// app.get to register route handler
+app.get('/todos', (req,res) =>{
+	Todo.find().then((todos) =>{
+		res.send({todos});
+	}), (e) =>{
+		res.status(400).send(e);
+	}
+});
+
 
 // so now you have a basic server setup
 app.listen(3000, () =>{
 	console.log('Started on port 3000');
 });
+
+
+module.exports = {app};
